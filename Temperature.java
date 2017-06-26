ca42d9951b72d2cdea38fc60796a8eaa84332a89
WEILANG2690 <william82628@outlook.com>
2017-06-26 16:24:25 +0800

    Update Temperature.java

--- Temperature.java
+++ Temperature.java
@@ -9,7 +9,7 @@ public class Temperature {
 			System.out.println("C=" + C);
 
 		} else if (index == 2) {
-			float F = T * (9 / 5) + 32; // 華氏 = 攝氏*(9/5)+32
+			float F = (T * (9 / 5)) + 32; // 華氏 = 攝氏*(9/5)+32
 			System.out.println("F=" + F);
 
 		} else {
