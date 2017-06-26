d0db7089751e164aa917de3b70c0a46ab9ce635c
WEILANG2690 <william82628@outlook.com>
2017-06-26 16:22:20 +0800

    Add files via upload

---
+++ H4EX19.java
@@ -0,0 +1,11 @@
+
+public class H4EX19 {
+
+	public static void main(String[] args) {
+		int week, day;
+		week = 285 / 7;
+		day = 285 % 7;
+		System.out.println("285天是"+week + "週又" + day + "天");
+	}
+
+}
