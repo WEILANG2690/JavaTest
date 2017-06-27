95eaa328c2f678c13a645d160a6b090f1fd1f546
WEILANG2690 <william82628@outlook.com>
2017-06-27 23:44:38 +0800

    Update For 迴圈 9X9.java

--- For 迴圈 9X9.java
+++ For 迴圈 9X9.java
@@ -5,7 +5,7 @@ public class TestFor {
 	public static void main(String[] args) {
 		for (int i = 1; i <= 9; i++) {
 			for (int j = 1; j <= 9; j++) {
-			System.out.printf("%dx%d=%2d\t",i,j,i*j);
+			System.out.printf("%dx%d=%2d\t",i,j,i*j);// %d 整數, %2d 2位
 		}
 		
         System.out.println();
