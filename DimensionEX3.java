16e963fc66695b2ac4fff20ade915d95cba99777
WEILANG2690 <william82628@outlook.com>
2017-06-28 20:42:16 +0800

    Add files via upload

---
+++ DimensionEX3.java
@@ -0,0 +1,16 @@
+package tw.WEILANG.myporject.Home;
+
+public class DimensionEX3 {
+	int[] data =new int[100000];
+	 int  x =(int)(Math.random()*42+1);
+	 
+	 
+ 
+	public static void main(String[] args) {
+		DimensionEX3 num = new DimensionEX3();
+		 //for(int i =1;i<=x;i++){
+			System.out.println("x="+num);
+		 //}
+	}
+	
+}
