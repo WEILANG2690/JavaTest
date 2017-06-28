4de6936f27c2ca63a76b2b763d869f70439763d1
WEILANG2690 <william82628@outlook.com>
2017-06-28 19:28:02 +0800

    Update Overload 多載.java

--- Overload 多載.java
+++ Overload 多載.java
@@ -3,10 +3,10 @@ package tw.WEILANG.myporject.Overload;
 public class TestOverloadEX1 {
 	
 
-	public  void plus(int  x1, int  y1 ){
+	public  void plus(int  x1, int  y1 ){ //int 型別
 	System.out.println("x1+y1="+(x1+y1));
 }
-	public void plus(double x2,double y2){
+	public void plus(double x2,double y2){ //double 型別
 		System.out.println("x2+y2="+(x2+y2));
 	}
 
