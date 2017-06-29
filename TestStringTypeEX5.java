cf06534ac23992299a0761fd91f953c6ce8137ad
WEILANG2690 <william82628@outlook.com>
2017-06-29 14:04:19 +0800

    Add files via upload

---
+++ TestStringTypeEX5.java
@@ -0,0 +1,14 @@
+package tw.WEILANG.myporject.Type;
+
+public class TestStringTypeEX5 {
+
+	public static void main(String[] args) {
+String data ="abcdefghij";
+System.out.println("data.charAt(5)="+data.charAt(5));
+System.out.println("data.length()="+data.length());
+System.out.println("substring(2, 5)="+data.substring(2, 5));
+System.out.println("data.indexOf('cd')=" + data.indexOf("cd"));
+System.out.println("data.toUpperCase()="+data.toUpperCase());
+	}
+
+}
