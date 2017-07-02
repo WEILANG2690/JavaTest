7681d75dde3e4f17eb323a100030abab836a64b4
WEILANG2690 <william82628@outlook.com>
2017-07-02 18:25:57 +0800

    Update MethodEX1(方法).java

--- MethodEX1(方法).java
+++ MethodEX1(方法).java
@@ -4,19 +4,21 @@ class Person {
 	public void sayHello() {
 		System.out.println("Hola");
 	}
-	public void sayBye(){
+
+	public void sayBye() {
 		System.out.println("Adios");
 	}
 }
 
-public class TestMethod {
+public class TestMethodEX1 {
 
 	public static void main(String[] args) {
 		Person mike = new Person();
-		
+
 		mike.sayHello();
 		mike.sayBye();
 		System.out.print("Finished");
 	}
 
 }
+
