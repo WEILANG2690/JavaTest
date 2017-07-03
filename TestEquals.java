bd958bfc780a0f3873e1ec8671afe1e22e99fc8a
WEILANG2690 <william82628@outlook.com>
2017-07-03 14:04:26 +0800

    Add files via upload

---
+++ TestEquals.java
@@ -0,0 +1,27 @@
+class car {
+	String brand = "Java"; //brand 品牌
+
+	public void drive() {
+		System.out.println("Drive a" + brand + "car");
+	}
+
+}
+
+public class TestEquals {
+
+	public static void main(String[] args) {
+		// car1:0x1234 -> 0x2345
+		car car1 = new car();
+		// car2:0x2345
+		car car2 = new car();
+
+		System.out.println("(car1==car2=)=" + (car1 == car2));
+		System.out.println("(car1==car2=)=" + (car1.equals(car2)));
+
+		car1 = car2;
+
+		System.out.println("(car1==car2=)_2=" + (car1 == car2));
+		System.out.println("(car1==car2=)_2=" + (car1.equals(car2)));
+	}
+
+}
