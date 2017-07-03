cb0f7387b5299aa4744b2c293da1be83568d3a83
WEILANG2690 <william82628@outlook.com>
2017-07-03 14:08:20 +0800

    Add files via upload

---
+++ TestEncapsulation.java
@@ -0,0 +1,34 @@
+class materoom {
+	private String name = "mery";
+	private int age =18;
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
+
+public class TestEncapsulation {
+
+	public static void main(String[] args) {
+		materoom setandget = new materoom();
+		setandget.setName("judy");
+		setandget.setAge(22);
+		String newname = setandget.getName();
+		int newage =setandget.getAge();
+		System.out.println("newname=" + newname);
+		System.out.println("newage=" + newage);
+	}
+
+}
