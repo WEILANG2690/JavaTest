c95fe6a89fa03a06436621088f25bb063658f660
WEILANG2690 <william82628@outlook.com>
2017-07-03 14:19:51 +0800

    Update Equals()和"=="的比較.java

--- Equals()和"=="的比較.java
+++ Equals()和"=="的比較.java
@@ -10,14 +10,15 @@ class car {
 public class TestEquals {
 
 	public static void main(String[] args) {
-		// car1:0x1234 -> 0x2345
+		// car1:0x1234
 		car car1 = new car();
 		// car2:0x2345
 		car car2 = new car();
 
 		System.out.println("(car1==car2=)=" + (car1 == car2));
 		System.out.println("(car1==car2=)=" + (car1.equals(car2)));
-
+                
+		// car1:0x1234 -> 0x2345
 		car1 = car2;
 
 		System.out.println("(car1==car2=)_2=" + (car1 == car2));
