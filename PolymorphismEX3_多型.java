b053977843440ff3d88bea4f110e8f94392b4d0c
WEILANG2690 <william82628@outlook.com>
2017-07-03 20:04:42 +0800

    Update PolymorphismEX3_多型.java

--- PolymorphismEX3_多型.java
+++ PolymorphismEX3_多型.java
@@ -29,25 +29,25 @@ class DoLaC extends DoLaA {
 }
 
 public class PolymorphismEX3 {
-	public void action(DoLaA D) {
+	public void action(DoLaA A) {
 
-		if (D instanceof DoLaB) {
-			DoLaB B1 = (DoLaB) D;
+		if (A instanceof DoLaB) {
+			DoLaB B1 = (DoLaB) A;
 			B1.sleep();
 			B1.jump();
 		}
-		if (D instanceof DoLaC) {
-			DoLaC C1 = (DoLaC) D;
+		if (A instanceof DoLaC) {
+			DoLaC C1 = (DoLaC) A;
 			C1.sleep();
 			C1.jump();
 		}
 	}
 
 	public static void main(String[] args) {
-		PolymorphismEX3 A = new PolymorphismEX3();
+		PolymorphismEX3 D = new PolymorphismEX3();
 
-		A.action(new DoLaB());
-		A.action(new DoLaC());
+		D.action(new DoLaB());
+		D.action(new DoLaC());
 
 	}
 
