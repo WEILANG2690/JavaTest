5f62521bdde807c00b635cf72f882dc3911a934f
WEILANG2690 <william82628@outlook.com>
2017-07-03 11:48:31 +0800

    Add files via upload

---
+++ TestSuperInheritance.java
@@ -0,0 +1,35 @@
+class Fish {
+	String name = "Nemo";
+
+	public Fish() {// this占用,需建立給17行繼承
+		super();
+		System.out.println("default action");
+	}
+
+	public Fish(String name) {
+		this.name = name;
+	}
+
+	 public void swim() {
+	 System.out.println("I can swim+" + name);
+	 }
+}
+
+class shark extends Fish {
+	public shark() { // 建構子
+		super("Dolly"); // 只能再建構子下第一行呼叫
+	}
+	public void action(){
+		super.swim();
+	}
+}
+
+public class TestSuperInheritance {
+
+	public static void main(String[] args) {
+		shark tigershark = new shark();
+		tigershark.action();
+		System.out.println("finished");
+	}
+
+}
