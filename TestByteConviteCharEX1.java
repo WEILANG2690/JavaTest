48169beecec80d9989197231c18e2c8ace19911a
WEILANG2690 <william82628@outlook.com>
2017-07-07 11:09:33 +0800

    Add files via upload

---
+++ TestByteConviteCharEX1.java
@@ -0,0 +1,34 @@
+package July07;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TestByteConviteCharEX1 {
+
+	public static void main(String[] args) throws IOException {
+
+		System.out.println("Please Input Your Message:(use Q or q to exit)");
+
+		// InputStream is1 = System.in;
+		// InputStreamReader isr1 = new InputStreamReader(is1);
+		// BufferedReader br1 = new BufferedReader(isr1);
+
+		BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+
+		String data;
+		while ((data = br1.readLine()) != null) {
+			if (data.equalsIgnoreCase("Q")) {
+				br1.close();
+				// isr1.close();
+				// is1.close();
+
+				System.out.println("see you next time");
+				System.exit(-1);
+			}
+			System.out.println("data=" + data);
+		}
+		
+	}
+
+}
